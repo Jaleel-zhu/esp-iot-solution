39fcfc1b31a50b1b755c1f2d5797b4f10888ebab
lvhaiyu <lvhaiyu@espressif.com>
2026-05-06 10:07:09 +0800

    feat(lcd): Adapt esp-lvgl-adapter related components for the ESP32-S31 target

--- components/display/tools/esp_lv_eaf_player/CHANGELOG.md
+++ components/display/tools/esp_lv_eaf_player/CHANGELOG.md
@@ -1,5 +1,9 @@
 # ChangeLog
 
+## v0.3.0 (2026-05-06)
+
+* Added ESP32-S31 support.
+
 ## v0.2.0
 
 * Add EAF transparency support.
--- examples/display/gui/common/hw_init/lcd/lcd_init_rgb.c
+++ examples/display/gui/common/hw_init/lcd/lcd_init_rgb.c
@@ -13,14 +13,42 @@
 
 static const char *TAG = "hw_lcd_init";
 
+#if CONFIG_IDF_TARGET_ESP32S3
 #define HW_LCD_PIXEL_CLOCK_HZ                   (18 * 1000 * 1000)
-#define HW_LCD_HSYNC                            (40)
-#define HW_LCD_HBP                              (40)
-#define HW_LCD_HFP                              (48)
-#define HW_LCD_VSYNC                            (23)
-#define HW_LCD_VBP                              (32)
-#define HW_LCD_VFP                              (13)
+#else
+#define HW_LCD_PIXEL_CLOCK_HZ                   (26 * 1000 * 1000)
+#endif
 
+#define HW_LCD_HSYNC                            (1)
+#define HW_LCD_HBP                              (40)
+#define HW_LCD_HFP                              (20)
+#define HW_LCD_VSYNC                            (1)
+#define HW_LCD_VBP                              (10)
+#define HW_LCD_VFP                              (5)
+
+#if CONFIG_IDF_TARGET_ESP32S31
+#define HW_LCD_RGB_VSYNC                        (GPIO_NUM_45)
+#define HW_LCD_RGB_HSYNC                        (GPIO_NUM_44)
+#define HW_LCD_RGB_DE                           (GPIO_NUM_43)
+#define HW_LCD_RGB_PCLK                         (GPIO_NUM_40)
+#define HW_LCD_RGB_DISP                         (GPIO_NUM_NC)
+#define HW_LCD_RGB_DATA0                        (GPIO_NUM_8)
+#define HW_LCD_RGB_DATA1                        (GPIO_NUM_9)
+#define HW_LCD_RGB_DATA2                        (GPIO_NUM_10)
+#define HW_LCD_RGB_DATA3                        (GPIO_NUM_11)
+#define HW_LCD_RGB_DATA4                        (GPIO_NUM_12)
+#define HW_LCD_RGB_DATA5                        (GPIO_NUM_13)
+#define HW_LCD_RGB_DATA6                        (GPIO_NUM_14)
+#define HW_LCD_RGB_DATA7                        (GPIO_NUM_15)
+#define HW_LCD_RGB_DATA8                        (GPIO_NUM_16)
+#define HW_LCD_RGB_DATA9                        (GPIO_NUM_17)
+#define HW_LCD_RGB_DATA10                       (GPIO_NUM_18)
+#define HW_LCD_RGB_DATA11                       (GPIO_NUM_19)
+#define HW_LCD_RGB_DATA12                       (GPIO_NUM_33)
+#define HW_LCD_RGB_DATA13                       (GPIO_NUM_34)
+#define HW_LCD_RGB_DATA14                       (GPIO_NUM_35)
+#define HW_LCD_RGB_DATA15                       (GPIO_NUM_36)
+#else
 #define HW_LCD_RGB_VSYNC                        (GPIO_NUM_3)
 #define HW_LCD_RGB_HSYNC                        (GPIO_NUM_46)
 #define HW_LCD_RGB_DE                           (GPIO_NUM_17)
@@ -42,6 +70,7 @@ static const char *TAG = "hw_lcd_init";
 #define HW_LCD_RGB_DATA13                       (GPIO_NUM_42)
 #define HW_LCD_RGB_DATA14                       (GPIO_NUM_2)
 #define HW_LCD_RGB_DATA15                       (GPIO_NUM_1)
+#endif
 
 #define HW_LCD_DATA_WIDTH                       (16)
 #define HW_LCD_BIT_PER_PIXEL                    (16)
@@ -56,7 +85,11 @@ esp_err_t hw_lcd_init(esp_lcd_panel_handle_t *panel_handle, esp_lcd_panel_io_han
         .clk_src = LCD_CLK_SRC_DEFAULT,
         .dma_burst_size = 64,
         .data_width = HW_LCD_DATA_WIDTH,
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(6, 0, 0)
+        .in_color_format = LCD_COLOR_FMT_RGB565,
+#else
         .bits_per_pixel = HW_LCD_BIT_PER_PIXEL,
+#endif
         .de_gpio_num = HW_LCD_RGB_DE,
         .pclk_gpio_num = HW_LCD_RGB_PCLK,
         .vsync_gpio_num = HW_LCD_RGB_VSYNC,
@@ -96,7 +129,9 @@ esp_err_t hw_lcd_init(esp_lcd_panel_handle_t *panel_handle, esp_lcd_panel_io_han
         },
         .flags.fb_in_psram = 1,
         .num_fbs = esp_lv_adapter_get_required_frame_buffer_count(tear_avoid_mode, rotation),
+#if CONFIG_IDF_TARGET_ESP32S3
         .bounce_buffer_size_px = HW_LCD_H_RES * HW_LCD_BOUNCE_BUFFER_HEIGHT,
+#endif
     };
     ESP_ERROR_CHECK(esp_lcd_new_rgb_panel(&panel_conf, &s_panel_handle));
     ESP_ERROR_CHECK(esp_lcd_panel_reset(s_panel_handle));
